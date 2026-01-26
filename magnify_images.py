25dc281c1e3f9e2a9c2785c056d6c65b6cccd006
Elfiawesome23 <elfiawesome23@gmail.com>
2026-01-26 19:01:18 +0800

    Add new image assets and update paths in JSON files; refactor image magnification script

--- magnify_images.py
+++ magnify_images.py
@@ -1,23 +1,32 @@
 from pathlib import Path
 from PIL import Image
-import os
 
-data = Path("./characters").resolve()
-
-exclude_dirs_set = ('.')
-
-for dirpath, dirs, files in os.walk(data):
-	dirs[:] = [d for d in dirs if not d.startswith(".")]
-	for filename in files:
-		file = Path(dirpath) / filename
+def _try_magnify_image(target_dir: Path, file: Path, asset_dir: Path) -> None:
 		if file.suffix != ".png":
-			continue
-		
-		
-		if not file.with_suffix("").name.endswith('-lrg'):
-			print(file)
-			img = Image.open(file)
-			new_size = (img.size[0]*8, img.size[1]*8)
-			img.resize(new_size, Image.Resampling.NEAREST)\
-				.save(str(file.with_suffix("")) + "-lrg" + file.suffix)
-			img.close()
+			return
+		relative_path = file.parent.relative_to(target_dir.parent)
+		folder = asset_dir / relative_path
+		folder.mkdir(exist_ok=True, parents=True)
+
+		print("magnifying: " + file.name)
+		img = Image.open(file)
+		new_size = (img.size[0]*8, img.size[1]*8)
+		img.resize(new_size, Image.Resampling.NEAREST)\
+			.save(folder / file.name)
+		img.close()
+
+def main():
+	ASSET_DIR = Path().resolve() / "assets"
+	
+	TARGET_DIRS = [Path(i).resolve() for i in [
+		"./characters/universal",
+		"./characters/engineer"
+	]]
+
+	for target_dir in TARGET_DIRS:
+		for filepath in target_dir.iterdir():
+			if filepath.is_file():
+				_try_magnify_image(target_dir, filepath, ASSET_DIR)
+
+if __name__ == "__main__":
+	main()
